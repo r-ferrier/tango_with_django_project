dbdf4e876f59b511924454e1a8d176b7bdae918c
“r-ferriergit config --global user.email “rosemary.ferrier@gmail.com <rosemary.ferrier@gmail.com>
2019-01-24 20:12:29 +0000

    finished all of chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+
+
+class Pages(admin.ModelAdmin):
+    # fields = ['title', 'category', 'url', 'views']
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Category)
+admin.site.register(Page, Pages)
