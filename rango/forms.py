99dc85ef6b7718724cda9bb12dd8e0ff7512b121
“r-ferriergit config --global user.email “rosemary.ferrier@gmail.com <rosemary.ferrier@gmail.com>
2019-02-09 13:01:16 +0000

    fixing some bugs

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
 
 class CategoryForm(forms.ModelForm):
@@ -17,12 +18,26 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
+                            help_text='Please enter the title of the page.')
     url = forms.URLField(max_length=200,
-                         help_text="Please enter the URL of the page.")
+                         help_text='Please enter the URL of the page.')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
 
         exclude = ('category',)
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Category (models.Model):
