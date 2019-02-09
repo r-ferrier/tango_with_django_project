1db8cebbf3572ebdd76751be927995ce7dc90e48
“r-ferriergit config --global user.email “rosemary.ferrier@gmail.com <rosemary.ferrier@gmail.com>
2019-02-09 14:45:20 +0000

    finished up to the end of chapter 9

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
@@ -46,7 +50,7 @@ def about(request):
     print(request.user)
     return render(request, 'rango/about.html', {})
 
-
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -60,7 +64,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
 
     try:
@@ -125,4 +129,38 @@ def register(request):
                   {'user_form': user_form,
                    'profile_form': profile_form,
                    'registered': registered})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            if 'username' == username and 'password' != password:
+                return HttpResponse("Your password looks like it could be wrong")
+            elif username != 'username':
+                return HttpResponse("No users registered with this username")
+    else:
+        return render(request, 'rango/login.html', {})
+
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -135,3 +135,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+
+LOGIN_URL = '/rango/login/'
